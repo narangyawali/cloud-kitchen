8cea25f3be8e7a171b8e51ab57c4a7bb2e81257a
naran gyawali <nryng123@gmail.com>
2023-12-21 21:25:06 +0545

    update: item card done

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -5,12 +5,12 @@ import { dish,chef } from '@/data'
 function Menu() {
   return (
    <>
-   <h1 className='text-2xl my-5'>Best food around your location</h1>
+   <h1 className='text-2xl my-5 ml-56 '>Best food around your location</h1>
    <section className='flex flex-wrap justify-around '>
     {[1,1,1,1,1,1,1,1,1,1,1].map(()=>{
         return(<>
         
-		<Card/>
+		<Card dishItem={dish} />
         </>)
     })}
    </section>
@@ -18,10 +18,22 @@ function Menu() {
   )
 }
 
-function Card(){
+function Card({dishItem}){
     return(<>
-    <div className='h-96 w-96 border border-blue-500 m-3'>
-    <h1>card</h1>
+    <div className='h-96 w-96 border border-blue-500 m-3 rounded-2xl'>
+    <img src={`img/${dishItem.img}`} alt="" className='w-full h-64  rounded-2xl' />
+    <div className='flex justify-between'>
+    	<h1 className='ml-3'> {dishItem.name}</h1>
+        <h1 className='mr-3'>{dishItem.stars}</h1>
+    </div>
+    <div className='flex justify-between'>
+        <h1 className='ml-3'>Tags: {dishItem.tags}</h1>
+        <h1 className='mr-3'>price:{dishItem.price}</h1>
+    </div>
+    <div className='flex justify-between'>
+	<h1 className='ml-3'>order</h1>	
+	<h1 className='mr-3'>add to Cart</h1>	
+    </div>
     </div>
     </>)
 }
