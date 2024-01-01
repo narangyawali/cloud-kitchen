58919e3b2b7c8e70279d3a77c48071fbebd60eae
naran gyawali <nryng123@gmail.com>
2024-01-01 17:34:57 +0545

    update:image upload

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { fetcher } from "@/lib";
+import { convertBase64, fetcher } from "@/lib";
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 
@@ -48,6 +48,8 @@ function Chef() {
   const [address, setAddresss] = useState("");
   const [isAvailable, setIsAvailable] = useState("");
 
+  const [show, setShow] = useState(false);
+
   const handleClick = async () => {
     const data = await fetch(`/api/chef/`, {
       method: "PATCH",
@@ -70,97 +72,171 @@ function Chef() {
   // const
   return (
     <>
-    <div className="px-64 pb-10">
-
-      <h1 className="text-red-500">Chef</h1>
-      <section classname="px-56 pb-10 mx-56 ">
-        <h1 className="text-2xl">Your Profile</h1>
-        <input
-          type="text"
-          placeholder="Full Name"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-          className="border border-black h-8 pl-2 my-2"
-        />
-        <br />
-        <input
-          type="text"
-          placeholder="Preferd cusine"
-          value={cusine}
-          onChange={(e) => {
-            setCusine(e.target.value);
-          }}
-          className="border border-black h-8 pl-2 my-2 "
-        />
-        <br />
-        <textarea
-          name="desc"
-          id=""
-          cols="30"
-          rows="10"
-          value={description}
-          onChange={(e) => {
-            setDescription(e.target.value);
-          }}
-          className="border border-blue-600 p-2"
-          placeholder="Some Description About chef"
-        ></textarea>
-        <br />
-        <input
-          type="text"
-          placeholder="Address"
-          value={address}
-          onChange={(e) => {
-            setAddresss(e.target.value);
-          }}
-          className="border border-black h-8 pl-2 my-2 "
-        />
-        <br />
-        <button className="h-10 border border-blue-500 rounded-xl text-xl p-1">
-          Get Location
-        </button>
-        <br />
-        <input
-          type="checkbox"
-          id="switch"
-          value={isAvailable}
-          onChange={(e) => {
-            setIsAvailable(e.target.value);
-          }}
-          className="h-5 w-5 mt-3"
-        />
-        <label htmlFor="switch	" className="ml-5">
-          Is Available
-        </label>
-        <br />
-        <input
-          type="button"
-          value="Update"
-          className="h-10 border cursor-pointer border-blue-500 rounded-xl text-xl p-1"
-          onClick={handleClick}
+      <div className="px-64 pb-10">
+        <h1 className="text-red-500">Chef</h1>
+        <section classname="px-56 pb-10 mx-56 ">
+          <h1 className="text-2xl">Your Profile</h1>
+          <input
+            type="text"
+            placeholder="Full Name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            className="border border-black h-8 pl-2 my-2"
           />
-        <div className="flex justify-between">
-          <h1 className="text-2xl">Your Menu</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Preferd cusine"
+            value={cusine}
+            onChange={(e) => {
+              setCusine(e.target.value);
+            }}
+            className="border border-black h-8 pl-2 my-2 "
+          />
+          <br />
+          <textarea
+            name="desc"
+            id=""
+            cols="30"
+            rows="10"
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            className="border border-blue-600 p-2"
+            placeholder="Some Description About chef"
+          ></textarea>
+          <br />
+          <input
+            type="text"
+            placeholder="Address"
+            value={address}
+            onChange={(e) => {
+              setAddresss(e.target.value);
+            }}
+            className="border border-black h-8 pl-2 my-2 "
+          />
+          <br />
           <button className="h-10 border border-blue-500 rounded-xl text-xl p-1">
-            Add One
+            Get Location
           </button>
-        </div>
-        <div className=" flex flex-wrap justify-evenly items-center">
-          {/* <MenuItem/>
+          <br />
+          <input
+            type="checkbox"
+            id="switch"
+            value={isAvailable}
+            onChange={(e) => {
+              setIsAvailable(e.target.value);
+            }}
+            className="h-5 w-5 mt-3"
+          />
+          <label htmlFor="switch	" className="ml-5">
+            Is Available
+          </label>
+          <br />
+          <div className="flex justify-between">
+            <input
+              type="button"
+              value="Update"
+              className="h-10 border cursor-pointer border-blue-500 rounded-xl text-xl p-1"
+              onClick={handleClick}
+            />
+            <button
+              onClick={() => {
+                setShow(!show);
+              }}
+              className="h-10 border border-blue-500 rounded-xl text-xl p-1"
+            >
+              Add One
+            </button>
+          </div>
+          <AddItem show={show} />
+          <h1 className="text-2xl">Your Menu</h1>
+          <div className=" flex flex-wrap justify-evenly items-center">
+            {/* <MenuItem/>
 				<MenuItem/>
 				<MenuItem/>
 				<MenuItem/>
 				<MenuItem/>
       <MenuItem/> */}
-        </div>
-      </section>
+          </div>
+        </section>
       </div>
     </>
   );
 }
 
+function AddItem({ show }) {
+
+  const [file, setFile]= useState("")
+  const handleChange = async(e)=>{
+    const f = e.target.files[0]
+    const fileData= await convertBase64(f)
+    setFile(fileData)
+    console.log(fileData);
+  }
+
+	const [name,setName]= useState("")
+	const [price,setPrice]= useState("")
+	const [description,setDescription]= useState("")
+
+  const upload = async()=>{
+
+		const data = await fetch(`/api/items/`,{
+      method:"POST",
+      headers:{
+        'Accept': 'application/json',
+      'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({
+   		name,
+      image:file,
+      price,
+      description 
+      })
+    })
+    // const status = await data.status()
+    const result = await data.json()
+    console.log(result);
+
+  }
+
+  if (true) {
+    return (
+      <>
+        <div className="border border-blue-500 h-56 w-96 mx-96 absolute top-56">
+          <h1>Add Item</h1>
+          <input
+          	value={name}
+            onChange={(e)=>{setName(e.target.value)}}
+            type="text"
+            placeholder="name"
+            className="border border-b-blue-500"
+          />
+          <input
+          	value={price}
+            onChange={(e)=>{setPrice(e.target.value)}}
+            type="number"
+            placeholder="price"
+            className="border border-b-blue-500"
+          />
+          <input
+          	value={description}
+            onChange={(e)=>{setDescription(e.target.value)}}
+            type="text"
+            placeholder="description"
+            className="border border-b-blue-500"
+          />
+          <input  onChange={handleChange} type="file" />
+          <br />
+          <input type="button" value="upload" onClick={upload} className="cursor-pointer border border-blue-500 h-7 w-28" />
+        </div>
+      </>
+    );
+  }
+}
 function MenuItem() {
   return (
     <>
