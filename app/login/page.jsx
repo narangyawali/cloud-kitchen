ea7635cbb7362c0e9213e5ce5158952bf091bc0d
Bipin acharya <jogo.hatake00@gmail.com>
2024-02-23 16:56:39 +0545

    s

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -64,13 +64,13 @@ export default function Login() {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         {/* <label htmlFor="email">Email</label> */}
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-        <input type="email" name="email"  value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='' className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        <input type="email" name="email"  value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='' className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
     
         <div className="mt-7 sm:mx-auto sm:w-full sm:max-w-sm">
         <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label> 
         {/* <label htmlFor="password">Password</label> */}
-        <input type="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='' className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        <input type="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='' className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
         
         <div className='flex'>
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -62,12 +62,12 @@ export default function Register() {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         {/* <label htmlFor="email">Email</label> */}
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-        <input type="text" name="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} id="" placeholder='' className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        <input type="text" name="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} id="" placeholder='' className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
         <div className="mt-7 sm:mx-auto sm:w-full sm:max-w-sm">
         <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
         {/* <label htmlFor="password">Password</label> */}
-        <input type="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='' className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+        <input type="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='' className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       
         </div>
         
