ae23fcc3570e6901350a22a21058283ba0f98596
Bipin acharya <jogo.hatake00@gmail.com>
2024-02-23 15:56:06 +0545

    register page:bpn/girija

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -61,13 +61,13 @@ export default function Login() {
     <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
 
     <div className='flex flex-col items-center justify-center w-full'>
-        <div className='max-w-2xl p-4'>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         {/* <label htmlFor="email">Email</label> */}
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
         <input type="email" name="email"  value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder='' className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
     
-        <div>
+        <div className="mt-7 sm:mx-auto sm:w-full sm:max-w-sm">
         <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label> 
         {/* <label htmlFor="password">Password</label> */}
         <input type="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='' className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -56,22 +56,25 @@ export default function Register() {
   return (
   <>
 
-    <h1 className='w-full mb-8 text-4xl text-center text-primary'>Register</h1>
+    {/* <h1 className='w-full mb-8 text-4xl text-center text-primary'>Register</h1> */}
+    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register your account</h2>
     <div className='flex flex-col items-center justify-center w-full'>
-        <div className='max-w-2xl p-4'>
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         {/* <label htmlFor="email">Email</label> */}
-        <input type="text" name="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} id="" placeholder='Input email' className='h-10 pl-3 border border-red-300 w-80 rounded-2xl' />
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <input type="text" name="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} id="" placeholder='' className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
         </div>
-        <div>
+        <div className="mt-7 sm:mx-auto sm:w-full sm:max-w-sm">
+        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
         {/* <label htmlFor="password">Password</label> */}
-        <input type="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='Input password' className='h-10 pl-3 my-5 border border-red-300 w-80 rounded-2xl ' />
+        <input type="password" name="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder='' className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
       
         </div>
-        <h1 className='text-2xl mb-4'>Register</h1>
+        
         <div className='flex '>
 
-        <input type="button" value="As Chef" name='chef' onClick={handleClick} className='h-8 border border-slate-600 cursor-pointer w-40 rounded-2xl text-xl bg-blue-300' />
-        <input type="button" value="As Customer"name='customer' onClick={handleClick} className='h-8 border border-slate-600 cursor-pointer w-40 rounded-2xl text-xl bg-blue-300' />
+        <input type="button" value="As Chef" name='chef' onClick={handleClick} className="m-3 flex w-full justify-center rounded-md bg-indigo-600 px-5 py-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" />
+        <input type="button" value="As Customer"name='customer' onClick={handleClick} className="m-3 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" />
         </div>
 				{
 
